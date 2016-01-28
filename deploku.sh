5e4ec2d0de33d2d20465fbdad31fd316e73f68e3
Georgy Vlasov <wlasowegor@gmail.com>
2016-01-29 04:57:05 +0500

    Checkout master and remove the extra branch after deployment

--- deploku.sh
+++ deploku.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Deploys current content of master branch to heroku
-git checkout -b include_css
+git checkout -B include_css
 git rebase master
 sass public/stylesheets/style.sass --update
-git add public/stylesheets/style.css
-git add public/stylesheets/style.css.map
+git add -f public/stylesheets/style.css
+git add -f public/stylesheets/style.css.map
 git commit --amend -m "Add stylesheets"
 git push -f heroku include_css:master
+git checkout master
+git branch -D include_css
